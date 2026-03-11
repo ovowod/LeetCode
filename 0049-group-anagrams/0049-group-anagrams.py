e8a61057addde21adbaf70ddacf8784fba58458e
owod <ovowod@gmail.com>
2026-03-11 22:03:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,5 +4,9 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = defaultdict(list)
         for st in strs:
-            anagrams[str(sorted(st))].append(st)
+            k = [0] * 26
+            for s in st:
+                k[ord(s) - 97] += 1
+            anagrams[str(k)].append(st)
+        
         return list(anagrams.values())
