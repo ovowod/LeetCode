bf717cf4d8995bad3e6f1998f8d1cda4089065d4
owod <ovowod@gmail.com>
2026-03-11 23:50:13 +0900

    Time: 0 ms (100%), Space: 19.3 MB (95.26%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,6 +1,17 @@
-from itertools import permutations
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        possible = [list(p) for p in list(permutations(nums, len(nums)))]
-        return possible
+        result = []
+        def permutations(ls):    
+            if len(ls) == len(nums):
+                result.append(ls.copy()) 
+                return
+
+            for i in range(len(nums)):
+                if nums[i] in ls:
+                    continue
+                ls.append(nums[i])
+                permutations(ls)
+                ls.pop()
+        
+        permutations([])
+        return result
