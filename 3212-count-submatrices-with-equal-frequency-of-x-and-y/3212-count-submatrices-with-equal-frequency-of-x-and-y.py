5ffc66aa8204aac837b77b2ac467640d0cf983f5
owod <ovowod@gmail.com>
2026-03-19 15:10:03 +0900

    Time: 1045 ms (36.56%), Space: 158 MB (24.73%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.py
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.py
@@ -1,19 +1,20 @@
 class Solution:
     def numberOfSubmatrices(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
-        dp = [[[0, 0] for _ in range(n + 1)] for _ in range(m + 1)]
+        X = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        Y = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
         cnt = 0
 
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                dp[i][j][0] = dp[i][j-1][0] + dp[i-1][j][0] - dp[i-1][j-1][0]
-                dp[i][j][1] = dp[i][j-1][1] + dp[i-1][j][1] - dp[i-1][j-1][1]
+                X[i][j] = X[i][j-1] + X[i-1][j] - X[i-1][j-1]
+                Y[i][j] = Y[i][j-1] + Y[i-1][j] - Y[i-1][j-1]
+
                 if grid[i-1][j-1] == "X":
-                    dp[i][j][0] += 1
+                    X[i][j] += 1
                 elif grid[i-1][j-1] == "Y":
-                    dp[i][j][1] += 1
+                    Y[i][j] += 1
                 
-                if dp[i][j][0] > 0 and dp[i][j][0] == dp[i][j][1]:
+                if X[i][j] == Y[i][j] and X[i][j] > 0:
                     cnt += 1
-        
         return cnt
